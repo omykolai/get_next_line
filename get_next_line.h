4616948cb0c3046ae08c7a1ef2a987693f203a1d
omykolai <mike1996@ukr.net>
2018-03-05 18:12:50 +0200

    no strjoin

--- get_next_line.h
+++ get_next_line.h
@@ -5,23 +5,21 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: omykolai <omykolai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/11/30 12:58:20 by omykolai          #+#    #+#             */
-/*   Updated: 2017/12/07 18:17:14 by omykolai         ###   ########.fr       */
+/*   Created: 2018/02/23 10:39:13 by omykolai          #+#    #+#             */
+/*   Updated: 2018/03/05 18:04:00 by omykolai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 100000
+# define BUFF_SIZE 100
 
-typedef struct	s_dat
+typedef struct	s_buff
 {
-	int			fd;
-	int			len;
-	char		buff[BUFF_SIZE + 1];
+	char		value[BUFF_SIZE + 1];
 	int			i;
-}				t_dat;
+}				t_buff;
 
-int				get_next_line(const int fd, char **line);
+int				get_next_line(int fd, char **line);
 
 #endif
